ea7996e24e86078159008d54610ff0f180996c69
babofitos <tehchoi@gmail.com>
2014-12-30 17:46:29 -0500

    generate thead tr from model

--- js/app.js
+++ js/app.js
@@ -14,8 +14,8 @@ $(function() {
                 $.each(nameColumns, function(index, value) {
                     var name = value;
                     attendance[name] = [];
-
-                    for (var i = 0; i <= 11; i++) {
+                    var numDays = model.getNumDays();
+                    for (var i = 0; i < numDays; i++) {
                         attendance[name].push(getRandom());
                     }
                 });
@@ -34,13 +34,16 @@ $(function() {
         },
         setAttendance: function(data) {
             localStorage.attendance = JSON.stringify(data);
+        },
+        getNumDays: function() {
+            return 12;
         }
     };
 
     var attendanceView = {
         init: function() {
+            this.$thead = $('thead tr');
             this.$tbody = $('table tbody');
-            this.$nameCol = $('.name-col');
             this.render();
         },
         makeCheckboxes: function(tr, day) {
@@ -59,7 +62,30 @@ $(function() {
         render: function() {
             this.attendance = octopus.getAttendance();
             this.$tbody.empty();
+            this.$thead.empty();
             this.renderTds();
+            this.renderThs();
+        },
+        renderThs: function() {
+            var ths = [];
+            var th = $('<th/>', {
+                class: 'name-col',
+                text: 'Student Name'
+            });
+            ths.push(th);
+            var numDays = octopus.getNumDays() + 1;
+            for (var i=1;i<numDays;i++) {
+            th = $('<th/>', {
+                text: i
+            });
+            ths.push(th);
+            }
+            th = $('<th/>', {
+                class: 'missed-col',
+                text: 'Days Missed-col'
+            });
+            ths.push(th);
+            this.$thead.append(ths);
         },
         renderTds: function() {
             for (var prop in this.attendance) {
@@ -113,6 +139,9 @@ $(function() {
             });
             model.setAttendance(data);
             attendanceView.render();
+        },
+        getNumDays: function() {
+            return model.getNumDays();
         }
     };
     octopus.init();
